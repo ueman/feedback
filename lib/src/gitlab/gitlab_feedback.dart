15f6b96e6ed25e381f24a54e1d6fd07b908c8506
Jonas Uekötter <dev@uekoetter.dev>
2020-04-28 15:37:50 +0200

    Docs + small improvements

--- lib/src/gitlab/gitlab_feedback.dart
+++ lib/src/gitlab/gitlab_feedback.dart
@@ -4,14 +4,17 @@ import 'dart:typed_data';
 import 'package:feedback/src/gitlab/file_upload_reponse.dart';
 import 'package:feedback/src/gitlab/gitlab_settings.dart';
 import 'package:feedback/src/gitlab/issue.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
+/// Tries to create a new issue with the given [feedback] and [pngImage] at
+/// the GitLab instance as defined in [settings].
+/// If successful it returns the link to the created issue.
+/// REMARKS: This method does not do any exception handling, so if unsuccessful
+/// it may return nothing or throw an exception.
 Future<String> gitlabFeedback(
   GitLabSettings settings,
-  BuildContext context,
   String feedback,
   Uint8List pngImage,
 ) async {

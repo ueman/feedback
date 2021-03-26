c6033bf85751be17e9c319261f7446b57f1b5b23
JamesCullum <JamesCullum@users.noreply.github.com>
2021-03-26 10:14:59 +0100

    Remove artifact from debugging

--- lib/src/l18n/localization.dart
+++ lib/src/l18n/localization.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:feedback/src/l18n/translation.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';

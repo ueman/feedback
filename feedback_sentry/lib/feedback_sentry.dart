59adaa21fc705e621914a60f2ef891aadd55cae6
Jonas Uekötter <dev@uekoetter.dev>
2021-09-07 15:27:07 +0200

    wip

---
+++ feedback_sentry/lib/feedback_sentry.dart
@@ -0,0 +1,33 @@
+library feedback_sentry;
+
+import 'package:feedback/feedback.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sentry/sentry.dart';
+export 'package:feedback/feedback.dart';
+
+extension SentryFeedback on FeedbackData {
+  void showAndUploadToSentry({Hub? hub}) {
+    this.show(sendToSentry(hub: hub));
+  }
+}
+
+@visibleForTesting
+OnFeedbackCallback sendToSentry({
+  Hub? hub,
+}) {
+  final realHub = hub ?? HubAdapter();
+
+  return (UserFeedback feedback) async {
+    final id = await realHub.captureMessage(feedback.text, withScope: (scope) {
+      scope.addAttachment(SentryAttachment.fromUint8List(
+        feedback.screenshot,
+        'screenshot.png',
+        contentType: 'image/png',
+      ));
+    });
+    await realHub.captureUserFeedback(SentryUserFeedback(
+      eventId: id,
+      comments: feedback.text + '\n${feedback.extra.toString()}',
+    ));
+  };
+}

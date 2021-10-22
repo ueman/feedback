d7bdded260d25a5452756a17c334e248d159e34d
Jonas Uekötter <dev@uekoetter.dev>
2021-10-22 16:13:47 +0200

    improve addons

--- feedback_sentry/lib/feedback_sentry.dart
+++ feedback_sentry/lib/feedback_sentry.dart
@@ -5,12 +5,27 @@ import 'package:flutter/foundation.dart';
 import 'package:sentry/sentry.dart';
 export 'package:feedback/feedback.dart';
 
-extension SentryFeedback on FeedbackData {
-  void showAndUploadToSentry({Hub? hub}) {
-    this.show(sendToSentry(hub: hub));
+/// Extension on [FeedbackController] in order to make
+/// it easier to call [showAndUploadToSentry].
+extension SentryFeedback on FeedbackController {
+  /// This method opens the feedback ui and the users feedback
+  /// is uploaded to Sentry after the user submitted his feedback.
+  /// [name] and [email] are optional. They are shown in the Sentry.io ui.s
+  void showAndUploadToSentry({
+    Hub? hub,
+    String? name,
+    String? email,
+  }) {
+    this.show(sendToSentry(
+      hub: hub,
+      name: name,
+      email: email,
+    ));
   }
 }
 
+/// See [SentryFeedback.showAndUploadToSentry].
+/// This is just [visibleForTesting].
 @visibleForTesting
 OnFeedbackCallback sendToSentry({
   Hub? hub,

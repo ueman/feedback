6fc335a29130c0a803f944592039d775612dae42
Jonas Uekötter <ueman@users.noreply.github.com>
2023-03-25 16:10:58 +0100

    update feedback_gitlab

--- feedback_gitlab/example/example.dart
+++ feedback_gitlab/example/example.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             ElevatedButton(
               onPressed: () {

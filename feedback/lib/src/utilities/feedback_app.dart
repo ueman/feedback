d91aac35e3b297cf6e47cbde87ed2b7509b77609
Xavier Chretien <chretienxavier42@gmail.com>
2023-10-03 15:24:31 -0400

    chore(dark-mode): Remove unused imports

--- feedback/lib/src/utilities/feedback_app.dart
+++ feedback/lib/src/utilities/feedback_app.dart
@@ -3,9 +3,7 @@
 import 'package:feedback/src/l18n/localization.dart';
 import 'package:feedback/src/theme/feedback_theme.dart';
 import 'package:feedback/src/utilities/media_query_from_window.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class FeedbackApp extends StatelessWidget {
   const FeedbackApp({
